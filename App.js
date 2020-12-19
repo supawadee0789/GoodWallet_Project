94bfe98129eacb29e8b630fa0dac0c51bb7026d6
supawadee khamnuengsithi <35362594+supawadee0789@users.noreply.github.com>
2020-12-19 20:42:07 +0700

    fix bug input number

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { TouchableWithoutFeedback ,Keyboard } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import StartScreen from "./screens/startScreen";
@@ -7,13 +7,19 @@ import AddWalletScreen from "./screens/AddWalletScreen";
 
 export default function App() {
   return (
-    <LinearGradient
-      colors={["#AF90F2", "#DF8D9F"]}
-      style={{ flex: 1, paddingTop: 15}}
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
     >
-      <AddWalletScreen/>
-      
-      {/* <StartScreen /> */}
-    </LinearGradient>
+      <LinearGradient
+        colors={["#AF90F2", "#DF8D9F"]}
+        style={{ flex: 1, paddingTop: 15 }}
+      >
+        <AddWalletScreen />
+
+        {/* <StartScreen /> */}
+      </LinearGradient>
+    </TouchableWithoutFeedback>
   );
 }
--- components/AddWalletCard.js
+++ components/AddWalletCard.js
@@ -2,28 +2,49 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const AddWalletCard = (props) => {
-    const [enteredValue,setEnterValue] = useState('');
-    const numberInputHandler = inputText =>{
-        setEnterValue(inputText.replace(/[- #*;,.<>\{\}\[\]\\\/]/gi, ''));
-    };
+  const [enteredValue, setEnteredValue] = useState("");
+  const numberInputHandler = (inputText) => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  };
   return (
     <View style={styles.card}>
-        <View style={styles.walletName}>
-            <TextInput style={{height: 40 ,fontSize:22,paddingBottom:15,color:'#A1A1A1',textAlign:'center'}} underlineColorAndroid="#A1A1A1"
-        placeholder="wallet name"/>
-        </View>
+      <View style={styles.walletName}>
+        <TextInput
+          style={{
+            height: 40,
+            fontSize: 22,
+            paddingBottom: 15,
+            color: "#A1A1A1",
+            textAlign: "center",
+          }}
+          underlineColorAndroid="#A1A1A1"
+          placeholder="wallet name"
+        />
+      </View>
 
-      <TextInput style={{fontSize:50,paddingBottom:15,color:'#B58FE7ff',textAlign:'center'}} 
+      <TextInput
+        style={{
+          fontSize: 50,
+          paddingBottom: 15,
+          color: "#B58FE7ff",
+          textAlign: "center",
+          fontWeight: "bold",
+        }}
         placeholder="0"
         placeholderTextColor="#B58FE7ff"
-        keyboardType = 'decimal-pad'
-        autoCompleteType ="cc-number"
+        keyboardType="decimal-pad"
         maxLength={9}
-        onChange={numberInputHandler}
+        onChangeText={numberInputHandler}
         value={enteredValue}
-        
-         />
-      <Text style={{ fontSize: 14, color: "#A890FE", fontWeight: "900" ,alignSelf:'center'}}>
+      />
+      <Text
+        style={{
+          fontSize: 14,
+          color: "#A890FE",
+          fontWeight: "900",
+          alignSelf: "center",
+        }}
+      >
         BATH
       </Text>
     </View>
@@ -36,13 +57,13 @@ const styles = StyleSheet.create({
     borderRadius: 34,
     height: 252,
     width: 309,
-    justifyContent:'space-between',
-    paddingBottom:53
+    justifyContent: "space-between",
+    paddingBottom: 53,
   },
-  walletName:{
-    alignItems:'center',
-    paddingVertical:18,
-  }
+  walletName: {
+    alignItems: "center",
+    paddingVertical: 18,
+  },
 });
 
 export default AddWalletCard;
